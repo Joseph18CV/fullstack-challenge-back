85201316395b12b1b58148503ba0e106aa42e479
Joseph18CV <josephvriesman16@gmail.com>
2023-05-24 13:09:56 -0300

    feat: basic project scope plus dependencies

---
+++ src/app.ts
@@ -0,0 +1,11 @@
+import express, { Application } from "express"
+import { handleErrors } from "./error"
+import cors from "cors"
+
+const app: Application = express()
+app.use(express.json())
+app.use(cors())
+
+app.use(handleErrors)
+
+export default app
---
+++ src/entities/client.entity.ts
@@ -0,0 +1,42 @@
+import { Entity, Column, PrimaryGeneratedColumn, CreateDateColumn, BeforeInsert, BeforeUpdate, OneToMany } from "typeorm"
+import { getRounds, hashSync } from "bcryptjs"
+import { Contact } from "./contacts.entity"
+
+@Entity("client")
+class Client {
+
+    @PrimaryGeneratedColumn("uuid")
+    id: number
+
+    @Column( { length: 127 } )
+    name: string
+
+    @Column( { length: 127, unique: true } )
+    email: string
+
+    @Column( { length: 127 } )
+    password: string
+
+    @Column( { length: 13, unique: true } )
+    telephone: string
+
+    @CreateDateColumn({type: "date"})
+    createdAt: string
+
+    @OneToMany(() => Contact, (contacts) => contacts.client)
+    contacts: Contact[]
+
+    @BeforeInsert()
+    @BeforeUpdate()
+    hashPassword(){
+        const isEncrypted = getRounds(this.password)
+        if(!isEncrypted){
+            this.password = hashSync(this.password, 10)
+        }
+    }   
+
+}
+
+export {
+    Client
+}
