e0c4befaaa27d89d0f167d822fdb4a9627530c9a
Joseph18CV <josephvriesman16@gmail.com>
2023-05-24 22:00:40 -0300

    add cors

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,14 @@
 import "reflect-metadata"
 import "express-async-errors"
 import express, { Application } from "express"
+import cors from "cors"
 import { handleErrors } from "./error"
 import clientRouters from "./routes/client.routes"
 import { loginRouters } from "./routes/login.routes"
 import contactRouters from "./routes/contacts.routes"
 
 const app: Application = express()
+app.use(cors())
 app.use(express.json())
 
 app.use("/client", clientRouters)
